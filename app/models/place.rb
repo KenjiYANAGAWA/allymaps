e4dbf818e620a97550dbec97b3ef26cc3eb6462a
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-25 15:40:23 +0900

    elevator status uncritical

--- app/models/place.rb
+++ app/models/place.rb
@@ -6,7 +6,7 @@ class Place < ApplicationRecord
   # unknown is NIL
   enum accessible_toilet: { no: 1, has_one: 2, has_many: 3 }, _suffix: true
   enum entrance: { one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true
-  enum elevator: {  no: 1, has: 2, not_applicable: 3 }, _suffix: true
+  enum elevator: {  no: 1, has: 2, uncritical: 3 }, _suffix: true
   enum priority_parking: { no: 1, has: 2 }, _suffix: true
   enum charging: { no: 1, has: 2 }, _suffix: true
 
