a98b88e5bcc442cc952210b307920670be2e4a1f
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-25 13:39:38 +0900

    fixed enum error

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,11 +3,12 @@ class Place < ApplicationRecord
   has_many_attached :photos
 
   # enum wheelchair_accessibility: { unknown: 0, inaccessible: 1, fair: 2, accessible: 3 }
-  enum accessible_toilet: { unknown: 0, no: 1, one: 2, many: 3 }
-  enum entrance: { unknown: 0, one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }
-  enum elevator: { unknown: 0, no: 1, yes: 2, na: 3 }
-  enum priority_parking: { unknown: 0, no: 1, yes: 2 }
-  enum charging: { unknown: 0, no: 1, yes: 2 }
+  # unknown is NIL
+  enum accessible_toilet: { no: 1, has_one: 2, has_many: 3 }, _suffix: true
+  enum entrance: { one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true
+  enum elevator: {  no: 1, has: 2, not_applicable: 3 }, _suffix: true
+  enum priority_parking: { no: 1, has: 2 }, _suffix: true
+  enum charging: { no: 1, has: 2 }, _suffix: true
 
   validates :name, :address, :accessible_toilet, :entrance, :elevator, :priority_parking, :charging, presence: true
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_033302) do
+ActiveRecord::Schema.define(version: 2022_05_25_035927) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +68,11 @@ ActiveRecord::Schema.define(version: 2022_05_25_033302) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.integer "accessible_toilet"
+    t.integer "entrance"
+    t.integer "elevator"
+    t.integer "priority_parking"
+    t.integer "charging"
   end
 
   create_table "reviews", force: :cascade do |t|
