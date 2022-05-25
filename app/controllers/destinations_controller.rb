cb4908b67b0a43b04cd583941212adef6a598630
Tirso Pascual <tpascualgarcia@gmail.com>
2022-05-25 11:23:24 +0900

    destinations def new

--- app/controllers/destinations_controller.rb
+++ app/controllers/destinations_controller.rb
@@ -1,6 +1,9 @@
 class DestinationsController < ApplicationController
   def new
-
+    @destination = Destination.new
+    @place = Place.find(params[:place_id])
+    authorize @destination
+    # raise
   end
 
   def create
