b1ac981c645e41113c134ed3ce0564aa7ac3e32a
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-25 17:05:06 +0900

    fixed column name

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_05_25_035927) do
+ActiveRecord::Schema.define(version: 2022_05_25_080339) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,10 +68,10 @@ ActiveRecord::Schema.define(version: 2022_05_25_035927) do
     t.float "longitude"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
-    t.integer "accessible_toilet"
+    t.integer "toilet"
     t.integer "entrance"
     t.integer "elevator"
-    t.integer "priority_parking"
+    t.integer "parking"
     t.integer "charging"
   end
 
