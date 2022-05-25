9b8605136cb6c37cc756b9f67dee19efb3a4ea2a
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-25 13:09:55 +0900

    model added validation presence for features of place model

--- app/models/place.rb
+++ app/models/place.rb
@@ -3,7 +3,13 @@ class Place < ApplicationRecord
   has_many_attached :photos
 
   # enum wheelchair_accessibility: { unknown: 0, inaccessible: 1, fair: 2, accessible: 3 }
-  validates :name, :address, presence: true
+  enum accessible_toilet: { unknown: 0, no: 1, one: 2, many: 3 }
+  enum entrance: { unknown: 0, one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }
+  enum elevator: { unknown: 0, no: 1, yes: 2, na: 3 }
+  enum priority_parking: { unknown: 0, no: 1, yes: 2 }
+  enum charging: { unknown: 0, no: 1, yes: 2 }
+
+  validates :name, :address, :accessible_toilet, :entrance, :elevator, :priority_parking, :charging, presence: true
 
   # geocoding
   geocoded_by :address
