fb5883fb3086d0e1f46a0b8b1c3b60a65d434dbe
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-26 01:03:15 +0900

    added no data status

--- app/models/place.rb
+++ app/models/place.rb
@@ -2,12 +2,12 @@ class Place < ApplicationRecord
   has_many :reviews, dependent: :destroy
   has_many_attached :photos
 
-  enum toilet: { no: 1, has_one: 2, has_many: 3 }, _suffix: true
-  enum entrance: { one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true
-  enum elevator: {  no: 1, has: 2, nonessential: 3 }, _suffix: true
-  enum parking: { no: 1, has: 2 }, _suffix: true
-  enum charging: { no: 1, has: 2 }, _suffix: true
-  enum area: { no: 1, some: 2, most: 3, all: 4 }, _suffix: true
+  enum toilet: { no_data: 0, no: 1, has_one: 2, has_many: 3 }, _suffix: true
+  enum entrance: { no_data: 0, one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true
+  enum elevator: { no_data: 0,  no: 1, has: 2, nonessential: 3 }, _suffix: true
+  enum parking: { no_data: 0, no: 1, has: 2 }, _suffix: true
+  enum charging: { no_data: 0, no: 1, has: 2 }, _suffix: true
+  enum area: { no_data: 0, no: 1, some: 2, most: 3, all: 4 }, _suffix: true
 
   validates :name, :address, presence: true
 
@@ -16,8 +16,20 @@ class Place < ApplicationRecord
   after_validation :geocode, if: :will_save_change_to_address?
 end
 
-# NIL means unknown
 # toilet means accessible toilets
 # parking emans priority parking
 # area means accessible area [None, some, most, all]
 # nonessential_elevator meaning the place does not have to have an elevator. e.g. parks
+#
+# CRUD for enum attributes
+#
+# # READ
+# place.area? -> returns boolean | false means unknown
+# place.area -> returns status key "some", "most", etc |  nil means unknown
+# place.some_area? -> return boolean
+# place.all_area? -> return boolean
+# place
+#
+# # UPDATE
+# place.update_attribute(:area, 'some') -> weird way found on google
+# place.area = 1
