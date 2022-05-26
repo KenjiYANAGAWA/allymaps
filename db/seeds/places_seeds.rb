81d486f3055ac1ed0b99cc4f6e4400b6ab4a7a17
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-26 22:02:18 +0900

    adding one by one

--- db/seeds/places_seeds.rb
+++ db/seeds/places_seeds.rb
@@ -10,8 +10,10 @@ place = Place.create!(
   parking: 'has',
   charging: 'no_data',
   area: 'some'
+  category: 'temple'
 )
 place.photos.attach(io: file, filename: 'place.jpeg')
+
 puts "..."
 file = File.open("app/assets/images/places/temples/kamakura_daibutsu.jpeg")
 place = Place.create!(
@@ -24,9 +26,34 @@ place = Place.create!(
   parking: 'has',
   charging: 'no_data',
   area: 'most'
+  category: 'temple'
 )
 place.photos.attach(io: file, filename: 'place.jpeg')
+
 puts "..."
+file = File.open("app/assets/images/places/landmarks/gunkanjima.png")
+place = Place.create!(
+  name:'Gunkanjima',
+  description: "Though it is officially called Hashima, the small mining island off the cost of Nagasaki came to be called Gunkanjima (Battleship Island) because the silhouette of the over-built island looks like a naval battleship.  Its eerie look even became the inspiration for a Bond villain’s hideout. Coal was first discovered on Gunkanjima in 1810 by the feudal lord of Saga, and after mining operations continued to build up, it was purchased by the Mitsubishi Corporation and permanently inhabited from 1887 onward. Though the island is only 480 meters long and 150 meters wide, it was home to 5000 residents – both miners and their families. The island was expanded and high sea walls were erected to protect the mining operations, residential space, schools, restaurants, shops, public bath and hospital. Operations were closed down in 1974 and the residents were required to leave the island. The buildings slowly gave way to the elements, leaving a haunting atmosphere. It is great to see local organizations help visitors with disabilities visit an otherwise inaccessible location.  The trip to Gunkanjima is a bit strenuous for some travelers, but is a unique opportunity that is worth the extra effort.",
+  address: 'Takashimamachi, Nagasaki, 851-1315, Japan',
+  city: 'Nagasaki',
+  toilet: 'has_one',
+  elevator: 'nonessential',
+  parking: 'no',
+  charging: 'no_data',
+  area: 'some'
+  category: 'landmark'
+)
+place.photos.attach(io: file, filename: 'place.jpeg')
+
+
+
+# toilet: { no_data: 0, no: 1, has_one: 2, has_many: 3 }, _suffix: true
+# charging: { no_data: 0, no: 1, has: 2 }, _suffix: true
+# area: { no_data: 0, no: 1, some: 2, most: 3, all: 4 }, _suffix: true
+# elevator: { no_data: 0,  no: 1, has: 2, nonessential: 3 }, _suffix: true
+# parking: { no_data: 0, no: 1, has: 2 }, _suffix: true
+
 # file = File.open("app/assets/images/places/temples/kamakura_daibutsu.jpeg")
 # place = Place.create!(
 #   name:'Kōtoku-in',
@@ -40,22 +67,3 @@ puts "..."
 #   area: 'most'
 # )
 # place.photos.attach(io: file, filename: 'place.jpeg')
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-# toilet: { no_data: 0, no: 1, has_one: 2, has_many: 3 }, _suffix: true
-# entrance: { no_data: 0, one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true elevator: { no_data: 0,  no: 1, has: 2, nonessential: 3 }, _suffix: true parking: { no_data: 0, no: 1, has: 2 }, _suffix: true
-# charging: { no_data: 0, no: 1, has: 2 }, _suffix: true
-# area: { no_data: 0, no: 1, some: 2, most: 3, all: 4 }, _suffix: true
