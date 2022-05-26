ff8d187371b7dd63ec96e57e457dcab1d4b02fc3
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-26 21:06:13 +0900

    seed in the middle

--- db/seeds/places_seeds.rb
+++ db/seeds/places_seeds.rb
@@ -1,17 +1,17 @@
 puts "..."
-  file = File.open("app/assets/images/places/temples/fushimiinari.jpeg")
-  place = Place.create!(
-    name:'Fushimi Inari Shrine',
-    description: 'Fushimi Inari Shrine is an iconic site in Kyoto with thousands of torii gates that wind up a forested mountain. With a bit of effort, visitors in wheelchairs can see the first stretch of the path and the first two shrines.',
-    address: '68 Fukakusa Yabunouchicho, Fushimi-ku, Kyoto, Kyoto, 612-0882, Japan',
-    city: 'Kyoto',
-    toilet: 'has_many',
-    elevator: 'has',
-    parking: 'has',
-    charging: 'no_data',
-    area: 'some'
-  )
-  place.photos.attach(io: file, filename: 'place.jpeg')
+file = File.open("app/assets/images/places/temples/fushimiinari.jpeg")
+place = Place.create!(
+  name:'Fushimi Inari Shrine',
+  description: 'Fushimi Inari Shrine is an iconic site in Kyoto with thousands of torii gates that wind up a forested mountain. With a bit of effort, visitors in wheelchairs can see the first stretch of the path and the first two shrines.',
+  address: '68 Fukakusa Yabunouchicho, Fushimi-ku, Kyoto, Kyoto, 612-0882, Japan',
+  city: 'Kyoto',
+  toilet: 'has_many',
+  elevator: 'has',
+  parking: 'has',
+  charging: 'no_data',
+  area: 'some'
+)
+place.photos.attach(io: file, filename: 'place.jpeg')
 puts "..."
 file = File.open("app/assets/images/places/temples/kamakura_daibutsu.jpeg")
 place = Place.create!(
@@ -26,6 +26,7 @@ place = Place.create!(
   area: 'most'
 )
 place.photos.attach(io: file, filename: 'place.jpeg')
+puts "..."
 # file = File.open("app/assets/images/places/temples/kamakura_daibutsu.jpeg")
 # place = Place.create!(
 #   name:'Kōtoku-in',
@@ -39,43 +40,22 @@ place.photos.attach(io: file, filename: 'place.jpeg')
 #   area: 'most'
 # )
 # place.photos.attach(io: file, filename: 'place.jpeg')
-# 10.times do
-#   puts "..."
-#   file = File.open("app/assets/images/places/fushimiinari.jpeg")
-#   place = Place.create!(
-#     name: Faker::JapaneseMedia::OnePiece.location,
-#     description: Faker::ChuckNorris.fact,
-#     address: japan_places.sample,
-#     toilet: Place.toilets.keys.sample,
-#     # entrance: Place.entrances.keys.sample,
-#     elevator: Place.elevators.keys.sample,
-#     parking: Place.parkings.keys.sample,
-#     charging: Place.chargings.keys.sample,
-#     area: Place.areas.keys.sample
-#   )
-#   place.photos.attach(io: file, filename: 'place.jpeg')
-#   # end
-#   puts "...created #{Place.count} places."
 
 
-# enum toilet: { no_data: 0, no: 1, has_one: 2, has_many: 3 }, _suffix: true
-# enum entrance: { no_data: 0, one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true
-# enum elevator: { no_data: 0,  no: 1, has: 2, nonessential: 3 }, _suffix: true
-# enum parking: { no_data: 0, no: 1, has: 2 }, _suffix: true
-# enum charging: { no_data: 0, no: 1, has: 2 }, _suffix: true
-# enum area: { no_data: 0, no: 1, some: 2, most: 3, all: 4 }, _suffix: true
 
-# t.string "name"
-# t.string "description"
-# t.string "address"
-# t.float "latitude"
-# t.float "longitude"
-# t.datetime "created_at", precision: 6, null: false
-# t.datetime "updated_at", precision: 6, null: false
-# t.integer "toilet"
-# t.integer "entrance"
-# t.integer "elevator"
-# t.integer "parking"
-# t.integer "charging"
-# t.integer "area"
-# t.string "city"
+
+
+
+
+
+
+
+
+
+
+
+
+# toilet: { no_data: 0, no: 1, has_one: 2, has_many: 3 }, _suffix: true
+# entrance: { no_data: 0, one_step: 1, two_or_more_steps: 2, ramp: 3, flat: 4 }, _suffix: true elevator: { no_data: 0,  no: 1, has: 2, nonessential: 3 }, _suffix: true parking: { no_data: 0, no: 1, has: 2 }, _suffix: true
+# charging: { no_data: 0, no: 1, has: 2 }, _suffix: true
+# area: { no_data: 0, no: 1, some: 2, most: 3, all: 4 }, _suffix: true
