c0c91bccce6b65a59578414f53ed153dfc40b5bd
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-26 16:51:06 +0900

    creating seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,31 +5,31 @@ User.destroy_all
 Place.destroy_all
 puts
 puts "Creating places..."
-Dir[File.join(Rails.root, 'db', 'seeds', 'places_seeds.rb')].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', 'places_seeds.rb')].each do |seed|
   load seed
 end
 puts "...created #{Place.count} places."
 puts
 puts "Creating users..."
-Dir[File.join(Rails.root, 'db', 'seeds', 'users_seeds.rb')].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', 'users_seeds.rb')].each do |seed|
   load seed
 end
 puts "...created #{User.count} users."
 puts
 puts "Creating reviews..."
-Dir[File.join(Rails.root, 'db', 'seeds', 'reviews_seeds.rb')].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', 'reviews_seeds.rb')].each do |seed|
   load seed
 end
 puts "...created #{Review.count} reviews."
 puts
 puts "Creating itineraries..."
-Dir[File.join(Rails.root, 'db', 'seeds', 'itineraries_seeds.rb')].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', 'itineraries_seeds.rb')].each do |seed|
   load seed
 end
 puts "...created #{Itinerary.count} itineraries."
 puts
 puts "Creating destinations..."
-Dir[File.join(Rails.root, 'db', 'seeds', 'destinations_seeds.rb')].sort.each do |seed|
+Dir[File.join(Rails.root, 'db', 'seeds', 'destinations_seeds.rb')].each do |seed|
   load seed
 end
 puts "...created #{Destination.count} destinations."
