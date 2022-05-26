c0a108f7cfe4af2caa3297274f325b1b03121c39
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-27 01:34:24 +0900

    fixed usj name

--- db/seeds/places_seeds.rb
+++ db/seeds/places_seeds.rb
@@ -124,7 +124,7 @@ puts "#{place.name} created"
 puts "..."
 file = File.open("app/assets/images/places/landmarks/usj.webp")
 place = Place.create!(
-  name:'Universal Studio Japan',
+  name:'Universal Studios Japan',
   description: "Universal Studios Japan is one of the most popular attractions in Osaka and offers excitement, famous characters, rides, shows and more. While it is fairly accessible in general, some of the most popular rides – including Harry Potter and the Forbidden Journey – are not accessible to guests with mobility difficulties. Universal Studios Japan remains one of this country’s more accessible tourist attractions. For starters, ample parking for wheelchair users is available, and the spaces are extra-long to allow room for loading and unloading equipment. USJ offers discounts at the gate to holders of red techo (an identification card for individuals with disabilities available to those covered by the National Health Insurance) and a companion. A special map detailing resources for those with special needs, called Support Book, is available in English and Japanes",
   address: '2-1-33 Sakurajima, Konohana-ku, Osaka, Osaka, 554-0031, Japan',
   city: 'Konohana-ku',
