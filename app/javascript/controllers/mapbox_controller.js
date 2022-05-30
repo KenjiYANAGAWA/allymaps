5e9f74085a21d7095b19ccc3770cb2553afb15eb
Kenji Yanagawa <kenchibi@gmail.com>
2022-05-30 14:49:48 +0900

    centered map

--- app/javascript/controllers/mapbox_controller.js
+++ app/javascript/controllers/mapbox_controller.js
@@ -13,11 +13,13 @@ export default class extends Controller {
 
     this.map = new mapboxgl.Map({
       container: this.element,
-      style: "mapbox://styles/mapbox/streets-v10"
+      style: "mapbox://styles/mapbox/streets-v10" //Add custom style later. Go to mapbox studio to create new style and get the link to paste here.
     })
 
     this.#addMarkersToMap()
     this.#fitMapToMarkers()
+
+    this.map.addControl(new MapboxGeocoder({ accessToken: mapboxgl.accessToken, mapboxgl: mapboxgl }))
   }
 
   #addMarkersToMap() {
