e129770069d6304a06550a7db370298da613a471
Kenji Yanagawa <kenchibi@gmail.com>
2022-06-01 15:00:35 +0900

    deleted destinations from seeds

--- db/seeds/destinations_seeds.rb
+++ db/seeds/destinations_seeds.rb
@@ -1,17 +1,17 @@
 puts "..."
-Destination.create!(
-  itinerary: User.find_by(email: '1@e.com').itineraries[0],
-  place: Place.find_by(name: 'Fushimi Inari Shrine')
-)
-Destination.create!(
-  itinerary: User.find_by(email: '1@e.com').itineraries[0],
-  place: Place.find_by(name: 'Kyoto Tower')
-)
-Destination.create!(
-  itinerary: User.find_by(email: '1@e.com').itineraries[0],
-  place: Place.find_by(name: 'Kinkakuji')
-)
-Destination.create!(
-  itinerary: User.find_by(email: '1@e.com').itineraries[0],
-  place: Place.find_by(name: 'Kyoto Arashiyama')
-)
+# Destination.create!(
+#   itinerary: User.find_by(email: '1@e.com').itineraries[0],
+#   place: Place.find_by(name: 'Fushimi Inari Shrine')
+# )
+# Destination.create!(
+#   itinerary: User.find_by(email: '1@e.com').itineraries[0],
+#   place: Place.find_by(name: 'Kyoto Tower')
+# )
+# Destination.create!(
+#   itinerary: User.find_by(email: '1@e.com').itineraries[0],
+#   place: Place.find_by(name: 'Kinkakuji')
+# )
+# Destination.create!(
+#   itinerary: User.find_by(email: '1@e.com').itineraries[0],
+#   place: Place.find_by(name: 'Kyoto Arashiyama')
+# )
