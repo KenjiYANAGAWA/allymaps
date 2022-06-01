4683e6bc27d09a18f7d3500c27aea0c2810aab77
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-01 09:36:40 +0900

    addresses also dont work, last button don work, when go back duplicates the button

--- app/javascript/packs/reorder.js
+++ app/javascript/packs/reorder.js
@@ -1,23 +1,28 @@
 // ---------------------------- ADD BUTTONS ---------------------------- //
 const destination_cards = document.querySelectorAll("#destination_card");
+// console.log("destinationss: ", destination_cards);
 destination_cards.forEach((card, index) => {
-  const nextCard = card.nextElementSibling;
-  console.log("nextcard: ", nextCard);
-  const fromAddress = card.dataset.address;
-  const toAddress = nextCard.dataset.address;
-  console.log(toAddress);
-  const btnHtml = `
-    <div class="d-flex justify-content-end">
-      <a href="https://www.google.com/maps/dir/${fromAddress}/${toAddress}" class="btn-route" target='_blank'>
-        <h6 class="mb-0"><i class="fas fa-map-marker-alt"></i>  Get directions</h6>
-      </a>
-    </div>`;
-  card.insertAdjacentHTML("afterend", btnHtml);
+  // console.log("cardddd: ", card);
+  if (Array.prototype.indexOf.call(destination_cards, card) !== destination_cards.length - 1) {   // means only NOT create the button after the last destination
+    const nextCard = destination_cards[index + 1];
+    // console.log("nextcard: ", nextCard);
+    const fromAddress = card.dataset.address;
+    const toAddress = nextCard.dataset.address;
+    // console.log("fromaddress: ", fromAddress);
+    // console.log("toaddress: ", toAddress);
+    const btnHtml = `
+      <div class="d-flex justify-content-end">
+        <a href="https://www.google.com/maps/dir/${fromAddress}/${toAddress}" class="btn-route" target='_blank'>
+          <h6 class="mb-0"><i class="fas fa-map-marker-alt"></i>  Get directions</h6>
+        </a>
+      </div>`;
+    console.log("asfddafadsfdsafdsfadsfdsfdsaf", card.parentElement.children);
+    console.log("length", destination_cards.length);
 
+    card.insertAdjacentHTML("afterend", btnHtml);
+
+  }
 })
-
-
-
 
 
 // ---------------------------- REORDER ---------------------------- //
@@ -28,12 +33,12 @@ const downButtons = document.querySelectorAll('#button-down');
 upButtons.forEach((upButton) => {
   upButton.addEventListener('click', (e) => {
     const card = e.currentTarget.parentElement.parentElement.parentElement;
-    if (!card.previousElementSibling) return
+    if (!card.previousElementSibling) return   // not do for the first destination
     const parent = card.parentElement;
     const previousSibling = card.previousElementSibling;
-    console.log("card: ", card);
-    console.log("parent: ", parent);
-    console.log("previousSibling: ", previousSibling);
+    // console.log("card: ", card);
+    // console.log("parent: ", parent);
+    // console.log("previousSibling: ", previousSibling);
     const index = Array.prototype.indexOf.call(parent.children, card)
     // parent is only 1 element. We want a collection so we do parent.children(whic is a collection). And call it on an Array.prototype insead on an HTML collection bc indexOf doens't seem to be working on a prototype
     console.log("index: ", index)
@@ -44,13 +49,16 @@ upButtons.forEach((upButton) => {
 downButtons.forEach((downButton) => {
   downButton.addEventListener('click', (e) => {
     const card = e.currentTarget.parentElement.parentElement.parentElement;
-    if (!card.nextElementSibling) return
-    console.log("card: ", card);
+    if (!card.nextElementSibling) return      // not do for the last destination
     const parent = card.parentElement;
     const nextNextSibling = card.nextElementSibling.nextElementSibling;
-    console.log(nextNextSibling);
+    // console.log("card: ", card);
+    // console.log("nextsibling: ", nextNextSibling);
     const index = Array.prototype.indexOf.call(parent.children, card)
     console.log("index: ", index)
     parent.insertBefore(card, nextNextSibling);   // insertAfter is not supported yer
   })
 })
+
+
+
