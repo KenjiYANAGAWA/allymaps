ceaa3ce879a01ed5d85688e67ee865b0b26d9bbf
Tirso Pascual <tpascualgarcia@gmail.com>
2022-06-01 19:07:09 +0900

    done it with arrows. still not able to save the order

--- app/javascript/packs/reorder.js
+++ app/javascript/packs/reorder.js
@@ -1,29 +1,34 @@
-// ---------------------------- ADD BUTTONS ---------------------------- //
-const destination_cards = document.querySelectorAll("#destination_card");
-// console.log("destinationss: ", destination_cards);
-destination_cards.forEach((card, index) => {
-  // console.log("cardddd: ", card);
-  if (Array.prototype.indexOf.call(destination_cards, card) !== destination_cards.length - 1) {   // means only NOT create the button after the last destination
-    const nextCard = destination_cards[index + 1];
-    // console.log("nextcard: ", nextCard);
-    const fromAddress = card.dataset.address;
-    const toAddress = nextCard.dataset.address;
-    // console.log("fromaddress: ", fromAddress);
-    // console.log("toaddress: ", toAddress);
-    const btnHtml = `
-      <div class="d-flex justify-content-end">
-        <a href="https://www.google.com/maps/dir/${fromAddress}/${toAddress}" class="btn-route" target='_blank'>
-          <h6 class="mb-0"><i class="fas fa-map-marker-alt"></i>  Get directions</h6>
-        </a>
-      </div>`;
-    console.log("asfddafadsfdsafdsfadsfdsfdsaf", card.parentElement.children);
-    console.log("length", destination_cards.length);
+// // ---------------------------- ADD BUTTONS ---------------------------- //
+// const destination_cards = document.querySelectorAll("#destination_card");
+// // console.log("destinationss: ", destination_cards);
 
-    card.insertAdjacentHTML("afterend", btnHtml);
+// const createButtons = () => {
+//   destination_cards.forEach((card, index) => {
+//     // console.log("cardddd: ", card);
+//     if (Array.prototype.indexOf.call(destination_cards, card) !== destination_cards.length - 1) {   // means only NOT create the button after the last destination
+//       const nextCard = destination_cards[index + 1];
+//       // console.log("nextcard: ", nextCard);
+//       const fromAddress = card.dataset.address;
+//       const toAddress = nextCard.dataset.address;
+//       // console.log("fromaddress: ", fromAddress);
+//       // console.log("toaddress: ", toAddress);
+//       const btnHtml = `
+//       <div id="directionButton" class="d-flex justify-content-end">
+//         <a href="https://www.google.com/maps/dir/${fromAddress}/${toAddress}" class="btn-route" target='_blank'>
+//           <h6 class="mb-0"><i class="fas fa-map-marker-alt"></i>  Get directions</h6>
+//         </a>
+//       </div>`;
+//       card.insertAdjacentHTML("afterend", btnHtml);
+//     }
+//   })
+// }
 
-  }
-})
-
+// const deleteButtons = () => {
+//   const directionButtons = document.querySelectorAll('#directionButton');
+//   directionButtons.forEach((button) => {
+//     button.remove();
+//   })
+// }
 
 // ---------------------------- REORDER ---------------------------- //
 const upButtons = document.querySelectorAll('#button-up');
@@ -32,23 +37,33 @@ const downButtons = document.querySelectorAll('#button-down');
 
 upButtons.forEach((upButton) => {
   upButton.addEventListener('click', (e) => {
-    const card = e.currentTarget.parentElement.parentElement.parentElement;
+    const card = e.currentTarget.parentElement.parentElement.parentElement.parentElement;
     if (!card.previousElementSibling) return   // not do for the first destination
     const parent = card.parentElement;
     const previousSibling = card.previousElementSibling;
-    // console.log("card: ", card);
-    // console.log("parent: ", parent);
-    // console.log("previousSibling: ", previousSibling);
+    console.log("card: ", card);
+    console.log("parent: ", parent);
+    console.log("previousSibling: ", previousSibling);
     const index = Array.prototype.indexOf.call(parent.children, card)
     // parent is only 1 element. We want a collection so we do parent.children(whic is a collection). And call it on an Array.prototype insead on an HTML collection bc indexOf doens't seem to be working on a prototype
     console.log("index: ", index)
     parent.insertBefore(card, previousSibling);
+    hideLastButton();
+    // change the url of the directionsButton
+    const directionButtons = document.querySelectorAll('#directionButton');
+    directionButtons.forEach((button, index) => {
+      const fromAddress = button.parentElement.previousElementSibling.children[0].dataset.address;
+      const toAddress = button.parentElement.parentElement.nextElementSibling.children[0].children[0].dataset.address;
+      console.log("fromAddress", index, ": ", fromAddress);
+      console.log("toAddress", index, ": ", toAddress);
+      button.href = `https://www.google.com/maps/dir/${fromAddress}/${toAddress}`;
+    })
   })
 })
 
 downButtons.forEach((downButton) => {
   downButton.addEventListener('click', (e) => {
-    const card = e.currentTarget.parentElement.parentElement.parentElement;
+    const card = e.currentTarget.parentElement.parentElement.parentElement.parentElement;
     if (!card.nextElementSibling) return      // not do for the last destination
     const parent = card.parentElement;
     const nextNextSibling = card.nextElementSibling.nextElementSibling;
@@ -56,9 +71,28 @@ downButtons.forEach((downButton) => {
     // console.log("nextsibling: ", nextNextSibling);
     const index = Array.prototype.indexOf.call(parent.children, card)
     console.log("index: ", index)
-    parent.insertBefore(card, nextNextSibling);   // insertAfter is not supported yer
+    parent.insertBefore(card, nextNextSibling);   // insertAfter is not supported yet
+    hideLastButton();
+    // change the url of the directionsButton
+    const directionButtons = document.querySelectorAll('#directionButton');
+    directionButtons.forEach((button, index) => {
+      const fromAddress = button.parentElement.previousElementSibling.children[0].dataset.address;
+      const toAddress = button.parentElement.parentElement.nextElementSibling.children[0].children[0].dataset.address;
+      console.log("fromAddress", index, ": ", fromAddress);
+      console.log("toAddress", index, ": ", toAddress);
+      button.href = `https://www.google.com/maps/dir/${fromAddress}/${toAddress}`;
+    })
   })
 })
 
 
-
+const hideLastButton = () => {
+  const directionButtons = document.querySelectorAll('#directionButton');
+  console.log("direction butonssss: ", directionButtons);
+  let lastButton = directionButtons[directionButtons.length - 1];
+  lastButton.style.display = 'none';
+  console.log("lastButton: ", lastButton);
+  const penultimoButton = directionButtons[directionButtons.length - 2];
+  penultimoButton.style.display = 'block';
+}
+hideLastButton();
